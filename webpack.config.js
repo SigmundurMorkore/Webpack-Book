b06bac6c7cefa0c8a8378fd554fdef619f8ee89c
mylbp2ps3 <sisomagmomunden@gmail.com>
2018-05-07 23:03:36 +0100

    Setting Up webpack-merge

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,33 @@
 require("dotenv").config()
+const merge = require("webpack-merge")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 
-module.exports = {
-  devServer: {
-    // Display only errors to reduce the amount of output.
-    stats: "errors-only",
+const parts = require("./webpack.parts")
 
-    // Parse host and port from env to allow customization.
-    //
-    // If you use Docker, Vagrant or Cloud9, set
-    // host: options.host || "0.0.0.0";
-    //
-    // 0.0.0.0 is available to all network devices
-    // unlike default "localhost".
-    host: process.env.HOST, // Defaults to "localhost"
-    port: process.env.PORT, // Defaults to 8080
-    open: false, // Open the page in browser
-    overlay: true // Overlay for capturing compilation related warnings and errors
-  },
+const commonConfig = merge([
+  {
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Webpack Book"
+      })
+    ]
+  }
+])
 
-  plugins: [new HtmlWebpackPlugin({ title: "Webpack Book" })]
+const productionConfig = merge([])
+
+const developmentConfig = merge([
+  parts.devServer({
+    // Customize host/port here if needed
+    host: process.env.HOST,
+    port: process.env.PORT
+  })
+])
+
+module.exports = mode => {
+  if (mode === "production") {
+    return merge(commonConfig, productionConfig, { mode })
+  }
+
+  return merge(commonConfig, developmentConfig, { mode })
 }
