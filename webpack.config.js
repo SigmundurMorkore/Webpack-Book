52279631fd9e622ed289cff86137e4eb917a830d
mylbp2ps3 <sisomagmomunden@gmail.com>
2018-05-07 23:52:19 +0100

    Connecting with Configuration

--- webpack.config.js
+++ webpack.config.js
@@ -11,19 +11,23 @@ const commonConfig = merge([
         title: "Webpack Book"
       })
     ]
-  },
-
-  parts.loadCSS()
+  }
 ])
 
-const productionConfig = merge([])
+const productionConfig = merge([
+  parts.extractCSS({
+    use: "css-loader"
+  })
+])
 
 const developmentConfig = merge([
   parts.devServer({
     // Customize host/port here if needed
     host: process.env.HOST,
     port: process.env.PORT
-  })
+  }),
+
+  parts.loadCSS()
 ])
 
 module.exports = mode => {
